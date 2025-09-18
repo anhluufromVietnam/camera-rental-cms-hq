263317be76a096582d7efac48aae9ce796ac536b
Bá Anh Lưu <lbanh@daihocthudo.edu.vn>
2025-09-18 03:51:20 +0000

    Title changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Booking Camera',
+  description: 'Created by Tâm Việt Quang',
+  generator: 'tvq.app',
 }
 
 export default function RootLayout({
